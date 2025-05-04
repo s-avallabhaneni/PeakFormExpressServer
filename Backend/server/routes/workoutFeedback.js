7db74be3dbc25cdc4f5e52046dcdedbf7c3a9e3c
vabhi2007 <1036614@lwsd.org>
2025-05-03 22:16:37 -0700

    Update workoutFeedback.js

--- Backend/server/routes/workoutFeedback.js
+++ Backend/server/routes/workoutFeedback.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const feedbackController = require('../controllers/workoutFeedbackController');
+const { verifyToken } = require('../middleware/authMiddleware');
 
 router.post('/users/:userId/feedback', verifyToken, feedbackController.createFeedback);
 router.delete('/feedback/:id', verifyToken, feedbackController.deleteFeedback);
