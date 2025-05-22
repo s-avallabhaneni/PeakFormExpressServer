a6f8fb65242cb895be08a3051352eb2983b2ca24
s-avallabhaneni <144369538+s-avallabhaneni@users.noreply.github.com>
2025-05-22 13:22:44 -0700

    Update exercises.js

--- Backend/server/routes/exercises.js
+++ Backend/server/routes/exercises.js
@@ -4,6 +4,7 @@ const exercisesController = require('../controllers/exercisesController');
 
 router.post('/templates/:templateId/exercises', exercisesController.createExercise);
 router.get('/templates/:templateId/exercises', exercisesController.getExercisesForTemplate);
+router.put('/exercises/:exerciseId', exercisesController.updateExercise);
 router.delete('/exercises/:exerciseId', exercisesController.deleteExercise);
 
 module.exports = router;
